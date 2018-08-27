56f495d997cd50c3754b1e22fb0928aabfa7cb34
Carlos HERNANDEZ CRESPO <carlos.hernandez_crespo.ext@nokia.com>
2018-08-27 11:43:27 +0200

    Version 3 finalizada y funcionando correctamente

--- src/ExamenJunio_2/Cliente.java
+++ src/ExamenJunio_2/Cliente.java
@@ -20,8 +20,138 @@ import java.util.logging.Logger;
 public class Cliente {
 
     public static void main(String[] args) {
-    
-    
+
+        try {
+
+            Scanner entrada = new Scanner(System.in);
+
+            Socket sc = new Socket("localhost", 6060);
+
+            DataInputStream entra = new DataInputStream(sc.getInputStream());
+            DataOutputStream sale = new DataOutputStream(sc.getOutputStream());
+
+            String nombre = "";
+            do {
+                System.out.println("Hola como te llamas?");
+
+                nombre = entrada.nextLine();
+
+            } while (entrada.equals(""));
+
+            sale.writeUTF(nombre);//W1
+
+            //imprimimos el saludo
+            System.out.println(entra.readUTF());//R1
+
+            boolean finJuego = false;
+            while (!finJuego) {
+
+                String cuantoDinero = entra.readUTF();//R2
+
+                int importe = -1;
+                do {
+                    System.out.println(cuantoDinero);
+                    try {
+                        importe = entrada.nextInt();
+                        entrada.nextLine();
+                    } catch (Exception e) {
+                    }
+
+                } while (importe < 1);
+
+                //mandamos el importe
+                sale.writeInt(importe);//W2
+
+                String aceptacion = entra.readUTF();//R3 
+
+                System.out.println(aceptacion);
+
+                if (aceptacion.contains("aceptada")) {
+
+                    //bucle saca cartas jugador
+                    boolean seHaPasadoCliente = false;
+                    boolean finSacarCartas = false;
+
+                    while (!seHaPasadoCliente && !finSacarCartas) {
+
+                        //leemos la carta sacada
+                        System.out.println(entra.readUTF());//R4
+
+                        //leemos y guardamos si nos hemos pasado o pregunta de continuar
+                        String pasadoContinuar = entra.readUTF();//R5
+
+                        if (pasadoContinuar.contains("PERDIDO")) {
+
+                            System.out.println(pasadoContinuar);
+                            seHaPasadoCliente = true;
+
+                        } else {
+
+                            String respuesta = "";
+                            do {
+                                System.out.println(pasadoContinuar);
+                                respuesta = entrada.nextLine().toUpperCase();//R6
+
+                            } while (!respuesta.equals("SI") && !respuesta.equals("NO"));
+
+                            //enviamos la respuesta
+                            sale.writeUTF(respuesta);//W3
+
+                            if (respuesta.equals("NO")) {
+
+                                finSacarCartas = true;
+                            }
+
+                        }
+                    }
+
+                    if (!seHaPasadoCliente) {
+
+                        //bucle saca cartas MAQUINA
+                        boolean seHaPasadoMaquina = false;
+                        boolean finSacaCartasMaquina = false;
+                        while (!finSacaCartasMaquina && !seHaPasadoMaquina) {
+
+                            String queHaPasado = entra.readUTF();//R7
+                            System.out.println(queHaPasado);
+
+                            if (queHaPasado.contains("GANADO")) {
+
+                                seHaPasadoMaquina = true;
+
+                            } else if (queHaPasado.contains("PERDIDO")) {
+
+                                finSacaCartasMaquina = true;
+                            }
+                        }
+                    }
+                    
+                    
+                    //respondemos si queremos seguir jugando                    
+                    String seguimos = entra.readUTF();
+                    String respuesta = "";
+                    do {                        
+                        System.out.println(seguimos);
+                        respuesta = entrada.nextLine().toUpperCase();                                      
+                    } while (!respuesta.equals("SI") && !respuesta.equals("NO"));
+                    
+                    sale.writeUTF(respuesta);
+
+                    if (respuesta.equals("NO")) {
+                        finJuego= true;
+                    }
+                    
+                }
+            }
+            
+            entra.close();
+            sale.close();
+            sc.close();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
 }
--- src/ExamenJunio_2/Servidor.java
+++ src/ExamenJunio_2/Servidor.java
@@ -30,15 +30,110 @@ public class Servidor implements Runnable {
 
     @Override
     public void run() {
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+        try {
+
+            DataInputStream entra = new DataInputStream(sc.getInputStream());
+            DataOutputStream sale = new DataOutputStream(sc.getOutputStream());
+
+            String nombre = entra.readUTF();//R1
+
+            //enviamos el saludo
+            sale.writeUTF("Hola " + nombre + " vamos a jugar al 7 y medio.");//W1
+
+            boolean finJuego = false;
+            while (!finJuego) {
+
+                sale.writeUTF("Cuanto dinero quieres apostar?");//W2  
+                int importe = entra.readInt();//R2
+
+                if (miBanca.getSaldo() > importe) {
+                    sale.writeUTF("Apuesta aceptada.");//W3
+
+                    miBanca.reservarApuesta(importe);//reservamos la apuesta del cliente
+
+                    //bucle saca cartas CLIENTE
+                    boolean finSacaCartasCliente = false;
+                    boolean seHaPasado = false;
+                    double puntuacionCliente = 0;
+                    while (!finSacaCartasCliente && !seHaPasado) {
+
+                        String carta = miBaraja.sacaCarta();
+
+                        sale.writeUTF("Has sacado la carta " + carta.split("_")[0] + " de " + carta.split("_")[1]);//W4
+
+                        puntuacionCliente = puntuacionCliente + miBaraja.valorCarta(carta);
+
+                        if (puntuacionCliente > 7.5) {
+
+                            sale.writeUTF("Has PERDIDO, te has pasado de 7.5");//W6
+                            seHaPasado = true;
+                            miBanca.ingresarAbanca(importe * 2);
+
+                        } else {
+
+                            sale.writeUTF("Quieres sacar otra carta?");//W5
+                            String otraCarta = entra.readUTF();//R3
+
+                            if (otraCarta.equals("NO")) {
+                                finSacaCartasCliente = true;
+                            }
+                        }
+
+                    }
+
+                    if (!seHaPasado) {
+
+                        //bucle saca cartas MAQUINA
+                        boolean seHaPasadoMaquina = false;
+                        boolean finSacaCartasMaquina = false;
+                        double puntuacionMaquina = 0;
+                        while (!seHaPasadoMaquina && !finSacaCartasMaquina) {
+
+                            String carta = miBaraja.sacaCarta();
+
+                            sale.writeUTF("La máquina ha sacado la carta " + carta.split("_")[0] + " de " + carta.split("_")[1]);//W6
+
+                            puntuacionMaquina = puntuacionMaquina + miBaraja.valorCarta(carta);
+
+                            if (puntuacionMaquina > 7.5) {
+
+                                seHaPasado = true;
+
+                                sale.writeUTF("Has GANADO, la máquina se ha pasado de 7.5");//W6
+
+                            } else if (puntuacionMaquina >= puntuacionCliente) {
+
+                                finSacaCartasMaquina = true;
+                                sale.writeUTF("Has PERDIDO, la máquina ha sacado una puntuación de " + puntuacionMaquina + " mientras que tu has sacado "+ puntuacionCliente);//W6
+                                miBanca.ingresarAbanca(importe * 2);
+                            }
+
+                        }
+                    }
+
+                    sale.writeUTF("Quieres volver a echar otra partida?");//W7
+                    String otraPartida = entra.readUTF();//R4
+                    if (otraPartida.equals("NO")) {
+                        finJuego = true;
+                    }
+
+                } else {
+
+                    sale.writeUTF("No hay saldo disponible. Por favor intentelo más tarde.");//W3
+
+                }
+
+            }
+
+            entra.close();
+            sale.close();
+            sc.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
 }
